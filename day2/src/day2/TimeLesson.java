8fe04a855fbe2227ab8b18b82e8b8e03caabcaee
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-04-27 14:39:14 +0900

    GameApp追加

--- day2/src/day2/TimeLesson.java
+++ day2/src/day2/TimeLesson.java
@@ -1,11 +1,13 @@
 package day2;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class TimeLesson {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		long start =System.currentTimeMillis();
 		String str="java";
 		for(int i=0;i<10000;i++) {
@@ -37,6 +39,17 @@ public class TimeLesson {
 		int y=c.get(Calendar.YEAR);
 		System.out.println("今年は"+y+"年です");
 		System.out.println("Hello");
+
+		SimpleDateFormat sdf=
+				new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+		Date d1 = sdf.parse("2022/04/27 01:23:45");
+		System.out.println(d1);
+
+		sdf=new SimpleDateFormat("yy/MM/dd");
+
+		String msg = sdf.format(d1);
+		System.out.println(msg);
 	}
 }
 
