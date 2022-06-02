7a8d464fdd123aa4a6fdea741b394ab12895dbfd
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-06-02 22:51:23 +0900

    6/2家

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N =sc.nextInt();
-		System.out.println(N%2==0?"even":"odd");
+		System.out.println(N%2== 0?"even":"odd");
 	}
 
 }
